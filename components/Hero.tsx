7983ba24146c2ca3b67af130924442ac183b17ad
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2023-02-23 16:04:03 +0800

    typewritter

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
 
-type Props = {}
+type Props = {};
 
 export default function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, The Name's Jayson",
+      "Guy-who-loves-coffee.tsx",
+      "<ButLovesToCodeMore />",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
   return (
-    <div>Hero</div>
-  )
+    <div>
+      <span>{text}</span>
+      <Cursor cursorColor="#F7AB0A" />
+    </div>
+  );
 }
