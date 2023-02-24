110ec0f336fceca866a43a04b7d3dc1e8b92c2b2
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2023-02-24 10:41:22 +0800

    backgroundcircle

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircle from "./BackgroundCircle";
 
 type Props = {};
 
@@ -14,9 +15,12 @@ export default function Hero({}: Props) {
     delaySpeed: 2000,
   });
   return (
-    <div>
-      <span>{text}</span>
-      <Cursor cursorColor="#F7AB0A" />
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircle />
+      <h1>
+        <span>{text}</span>
+        <Cursor cursorColor="#F7AB0A" />
+      </h1>
     </div>
   );
 }
